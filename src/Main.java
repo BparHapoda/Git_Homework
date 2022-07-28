99fd0bc463effb527caa6ee4279af1c1a7110761
Denis.Boronin <cddvdrom@ya.ru>
2022-07-28 14:01:42 +0300

    test lambda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hello wojkkkiioioiijjrld!");
+        int[] arr = {1,23,4};
+        for (int x :arr
+             ) {
+
+        }
+
+
     }
+
+    interface Compar{
+        boolean compar (int x);}
 }
